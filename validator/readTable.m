c37b6e7065cd13fe013234f0ef05d0f5fd595e8c
Jaromír Beneš <jaromir.benes@gmail.com>
2023-08-01 09:33:53 +0200

    Update readTable.m

--- validator/readTable.m
+++ validator/readTable.m
@@ -1,7 +1,14 @@
 
     specsFile = "input-data-validator.csv";
-    imo = detectImportOptions(specsFile, "fileType", "delimitedText", "delimiter", ",", "textType", "string");
+    options = {"fileType", "delimitedText", "delimiter", ",", "textType", "string"};
+    try
+        imo = detectImportOptions(specsFile, options{:}, "variableNamingRule", "preserve");
+    catch
+        imo = detectImportOptions(specsFile, options{:}, "preserveVariableNames", true);
+    end
     imo.VariableTypes(:) = {'string'};
     t = readtable(specsFile, imo);
+    diary output.txt
     disp(t.Properties.VariableNames)
-    
+    diary off
+
